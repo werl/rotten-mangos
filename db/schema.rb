b2235073f007e65174be4b2e187e385740d2a2ed
werl <peter@werl.me>
2016-06-26 18:06:57 -0700

    Renamed the password_diges to password_digest

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160622230851) do
+ActiveRecord::Schema.define(version: 20160627003214) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -36,13 +36,7 @@ ActiveRecord::Schema.define(version: 20160622230851) do
   add_index "reviews", ["movie_id"], name: "index_reviews_on_movie_id"
   add_index "reviews", ["user_id"], name: "index_reviews_on_user_id"
 
-  create_table "users", force: :cascade do |t|
-    t.string   "email"
-    t.string   "password_diges"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
-    t.string   "firstname"
-    t.string   "lastname"
-  end
+# Could not dump table "users" because of following NoMethodError
+#   undefined method `[]' for nil:NilClass
 
 end
